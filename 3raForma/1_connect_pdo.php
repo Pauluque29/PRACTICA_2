0e85edd198c3b32e105d143ad7062730144907e1
juanpaul29 <128315550+juanpaul29@users.noreply.github.com>
2023-04-23 22:00:43 -0400

    La practica 2 de la materia TEW-500 se realizo con éxito

---
+++ 3raForma/1_connect_pdo.php
@@ -0,0 +1,15 @@
+<?php
+include "../cabecera.php";
+echo $titulo[1];
+$hostname = "localhost";
+$usuario = "root";
+$password = "juanpaul29"; 
+try {
+$conn = new PDO("mysql:host=$hostname", $usuario, $password);
+// Establecer el modo de error PDO en excepción
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Conectado exitosamente con PDO";
+} catch(PDOException $e) {
+echo "La conexión falló: " . $e->getMessage();
+} 
+?>
